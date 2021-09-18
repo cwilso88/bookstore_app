dc06e7f797908d883f7a65dd828429641423ba05
cwilso88 <chasity.wilson4@gmail.com>
2021-09-18 17:31:54 -0400

    check circle for list

--- src/components/Mylist.js
+++ src/components/Mylist.js
@@ -1,5 +1,6 @@
 import react from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle } from "@fortawesome/free-regular-svg-icons";
 
 const Mylist = () => {
     return (
@@ -7,20 +8,24 @@ const Mylist = () => {
             <h3>My List of Books</h3>
             <hr />
             <ul>
-                <li>Clean Code<span className="delete-btn">Delete</span></li>
+                <li><span className="check-circle"><FontAwesomeIcon icon={faCheckCircle} /></span>Clean Code <span className="delete-btn">Delete</span></li>
             </ul>
             <ul>
-                <li>Clean Code<span className="delete-btn">Delete</span></li>
+                <li><span className="check-circle"><FontAwesomeIcon icon={faCheckCircle} /></span>Clean Code <span className="delete-btn">Delete</span></li>
             </ul>
             <ul>
-                <li>Clean Code<span className="delete-btn">Delete</span></li>
+                <li><span className="check-circle"><FontAwesomeIcon icon={faCheckCircle} /></span>Clean Code <span className="delete-btn">Delete</span></li>
             </ul>
             <ul>
-                <li>Clean Code<span className="delete-btn">Delete</span></li>
+                <li><span className="check-circle"><FontAwesomeIcon icon={faCheckCircle} /></span>Clean Code <span className="delete-btn">Delete</span></li>
             </ul>
             <ul>
-                <li>Clean Code<span className="delete-btn">Delete</span></li>
+                <li><span className="check-circle"><FontAwesomeIcon icon={faCheckCircle} /></span>Clean Code <span className="delete-btn">Delete</span></li>
             </ul>
+            <ul>
+                <li><span className="check-circle"><FontAwesomeIcon icon={faCheckCircle} /></span>Clean Code <span className="delete-btn">Delete</span></li>
+            </ul>
+            
             <div className="close-list">Close List</div>
         </section>
     )
