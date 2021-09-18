6b5d5d8c0592866e2f25e059acdf7d0139f8a55c
cwilso88 <chasity.wilson4@gmail.com>
2021-09-18 14:53:21 -0400

    Mylist component created

---
+++ src/Mylist.js
@@ -0,0 +1,14 @@
+import react from "react";
+
+const Mylist = () => {
+    return (
+        <section id="myList">
+            <h3>My List of Books</h3>
+            <ul>
+                <li>Clean Code</li>
+            </ul>
+        </section>
+    )
+}
+
+export default Mylist;
