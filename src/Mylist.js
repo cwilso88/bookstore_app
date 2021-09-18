73e009da1197e31fcd791bad441ea3312f3ab426
cwilso88 <chasity.wilson4@gmail.com>
2021-09-18 15:01:26 -0400

    Create ul book list

--- src/Mylist.js
+++ src/Mylist.js
@@ -4,8 +4,21 @@ const Mylist = () => {
     return (
         <section id="myList">
             <h3>My List of Books</h3>
+            <hr />
             <ul>
-                <li>Clean Code</li>
+                <li>Clean Code<span>Delete</span></li>
+            </ul>
+            <ul>
+                <li>Clean Code<span>Delete</span></li>
+            </ul>
+            <ul>
+                <li>Clean Code<span>Delete</span></li>
+            </ul>
+            <ul>
+                <li>Clean Code<span>Delete</span></li>
+            </ul>
+            <ul>
+                <li>Clean Code<span>Delete</span></li>
             </ul>
         </section>
     )
