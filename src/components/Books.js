4f26bd2f2ec3a6c137980832c7947766fd4ff725
cwilso88 <chasity.wilson4@gmail.com>
2021-09-18 23:14:21 -0400

    background image css

--- src/components/Books.js
+++ src/components/Books.js
@@ -4,7 +4,7 @@ const Books = () => {
     return (
        <section id="allBooks">
            <div className="book-container">
-                <div className="book">
+                <div className="book" style={{backgroundImage: 'url(https://images-na.ssl-images-amazon.com/images/I/41yafGMO+rL._SX376_BO1,204,203,200_.jpg)', backgroundSize: 'cover'}}>
 
                 </div>
            </div>
