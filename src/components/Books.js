e334200bee83eaed1aa8adf47d0188274528f292
cwilso88 <chasity.wilson4@gmail.com>
2021-09-19 03:16:33 -0400

    added a container around the booklist

--- src/components/Books.js
+++ src/components/Books.js
@@ -2,53 +2,55 @@ import react from "react";
 
 const Books = () => {
     return (
-       <section id="allBooks">
-           <div className="book-container">
-                <div className="book" style={{backgroundImage: 'url(https://images-na.ssl-images-amazon.com/images/I/41yafGMO+rL._SX376_BO1,204,203,200_.jpg)', backgroundSize: 'cover'}}>
+         <div className="container">
+          <section id="allBooks">
+               <div className="book-container">
+                    <div className="book" style={{backgroundImage: 'url(https://images-na.ssl-images-amazon.com/images/I/41yafGMO+rL._SX376_BO1,204,203,200_.jpg)', backgroundSize: 'cover'}}>
 
-                </div>
-           </div>
-           <div className="book-container">
-                <div className="book">
+                    </div>
+               </div>
+               <div className="book-container">
+                    <div className="book">
 
-                </div>
-           </div>
-           <div className="book-container">
-                <div className="book">
+                    </div>
+               </div>
+               <div className="book-container">
+                    <div className="book">
 
-                </div>
-           </div>
-           <div className="book-container">
-                <div className="book">
+                    </div>
+               </div>
+               <div className="book-container">
+                    <div className="book">
 
-                </div>
-           </div>
-           <div className="book-container">
-                <div className="book">
+                    </div>
+               </div>
+               <div className="book-container">
+                    <div className="book">
 
-                </div>
-           </div>
-           <div className="book-container">
-                <div className="book">
+                    </div>
+               </div>
+               <div className="book-container">
+                    <div className="book">
 
-                </div>
-           </div>
-           <div className="book-container">
-                <div className="book">
+                    </div>
+               </div>
+               <div className="book-container">
+                    <div className="book">
 
-                </div>
-           </div>
-           <div className="book-container">
-                <div className="book">
+                    </div>
+               </div>
+               <div className="book-container">
+                    <div className="book">
 
-                </div>
-           </div>
-           <div className="book-container">
-                <div className="book">
+                    </div>
+               </div>
+               <div className="book-container">
+                    <div className="book">
 
-                </div>
-           </div>
-       </section>
+                    </div>
+               </div>
+          </section>
+       </div>
     )
 }
 
