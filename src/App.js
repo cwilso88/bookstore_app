6b74694b263af52c95a4c5139cb8e9fee3f04730
cwilso88 <chasity.wilson4@gmail.com>
2021-09-19 16:11:44 -0400

    created modal css and imported to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import Books from './components/Books';
 import Mylist from './components/Mylist';
 import './css/Books.css';
-import './css/Mylist.css'
+import './css/Mylist.css';
+import './css/Modal.css';
 
 function App() {
   return (
