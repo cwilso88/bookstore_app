330265198ca463ba7cde2316382d3ba82a45f904
cwilso88 <chasity.wilson4@gmail.com>
2021-09-22 16:54:35 -0400

    booksDataReducer array 1st item

---
+++ src/reducers/booksDataReducer.js
@@ -0,0 +1,13 @@
+const booksDataReducer = () => {
+    return [
+        {
+            id: 1,
+            title: 'Clean Code', 
+            author: 'Robert C. Martin',
+            category: 'Information Technology',
+            published: '2008',
+            description: `Even bad code can function. But if code isn't clean, it can bring a development organization to its knees. Every year, countless hours and significant resources are lost because of poorly written code. ...`,
+            coverURL: 'https://images-na.ssl-images-amazon.com/images/I/41yafGMO+rL._SX376_BO1,204,203,200_.jpg'
+        }
+    ]
+}
