69f23b10ef1e13e19727ff529d93fd0b1fc4e268
cwilso88 <chasity.wilson4@gmail.com>
2021-09-22 05:50:02 -0400

    Installed Redux

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,38 +4,48 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons'
 
 
 const Modal = () => {
-    return(
-        <section id="modal" className="active">
-            <div className="modal-container">
-                <FontAwesomeIcon icon={faTimes} />
-                <div className="modal-grid">
-                    <div className="images">
-                        <div className="cover" style={{backgroundImage: 'url(https://images-na.ssl-images-amazon.com/images/I/41yafGMO+rL._SX376_BO1,204,203,200_.jpg)', backgroundSize: 'cover'}}>
-                            
-                        </div>
-                    </div>
-                        <div className="info">
-                            <h2>Title</h2>
-                            <div className="info-line">
-                                <span className="bold">Author:</span>
-                                Robert C. Martin
-                            </div>
-                            <div className="info-line">
-                                <span className="bold">Category:</span>
-                                Information Technology
-                            </div>
-                            <div className="info-line">
-                                <span className="bold">Published:</span>
-                                2008
-                            </div>
-                            <p className="description">
-                                Even bad code can function. But if code isn't clean, it can bring a development organization to its knees. Every year, countless hours and significant resources are lost because of poorly written code. ...
-                            </p>
-                        </div>
-                    </div>
-                </div>
-        </section>
-    )
+    return (
+      <section id="modal" className="active">
+        <div className="modal-container">
+          <div className="close-btn">
+            <FontAwesomeIcon icon={faTimes} />
+          </div>
+          <div className="modal-grid">
+            <div className="images">
+              <div
+                className="cover"
+                style={{
+                  backgroundImage:
+                    "url(https://images-na.ssl-images-amazon.com/images/I/41yafGMO+rL._SX376_BO1,204,203,200_.jpg)",
+                  backgroundSize: "cover",
+                }}
+              ></div>
+            </div>
+            <div className="info">
+              <h2>Title</h2>
+              <div className="info-line">
+                <span className="bold">Author:</span>
+                Robert C. Martin
+              </div>
+              <div className="info-line">
+                <span className="bold">Category:</span>
+                Information Technology
+              </div>
+              <div className="info-line">
+                <span className="bold">Published:</span>
+                2008
+              </div>
+              <p className="description">
+                Even bad code can function. But if code isn't clean, it can
+                bring a development organization to its knees. Every year,
+                countless hours and significant resources are lost because of
+                poorly written code. ...
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
 }
 
 export default Modal;
