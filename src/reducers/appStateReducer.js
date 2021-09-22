585ec7f18b363e4ecee439ead20cf027fa236ffb
cwilso88 <chasity.wilson4@gmail.com>
2021-09-22 06:37:15 -0400

    ADD BOOK CASE

---
+++ src/reducers/appStateReducer.js
@@ -0,0 +1,37 @@
+const initialState = {
+    myList: [],
+    openInfoBook: {},
+    popupOpen: false,
+    listOpen: false
+};
+
+let newState;
+
+const AppStateReducer = (state = initialState, action) => {
+    switch (action.type) {
+        // ADD BOOK CASE
+        case 'ADD_BOOK':
+            var myList = [...state.myList, 
+                action.payload];
+            
+            newState = Object.assign({}, state, {
+                myList: myList });
+            
+            return newState;
+            break;
+
+            // REMOVE BOOK CASE
+        case 'REMOVE_BOOK':
+            var myList = 
+            
+            newState = Object.assign({}, state, {
+                myList: myList });
+            
+            return newState;
+            break;
+    
+        default:
+            return state;
+            break;
+    }
+}
