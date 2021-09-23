22e850086dd2502402d7bb766596a6c7b8826374
cwilso88 <chasity.wilson4@gmail.com>
2021-09-23 18:06:45 -0400

    imported Provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
+import allReducers from './reducers/allReducers';
 
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={createStore(allReducers)}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
