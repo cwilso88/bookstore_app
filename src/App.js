37eb70e76a8f003b571f043dfbccb2c79669c328
cwilso88 <chasity.wilson4@gmail.com>
2021-09-24 18:31:08 -0400

    styles for the Article

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import Books from './components/Books';
 import Mylist from './components/Mylist';
 import Modal from './components/Modal';
 import Header from './components/Header';
+import Article from './components/Article';
 import './css/Books.css';
 import './css/Mylist.css';
 import './css/Modal.css';
 import './css/Header.css';
+import './css/Article.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Article />
       <Books />
       <Mylist />
       <Modal />
---
+++ src/components/Article.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Article = () => {
+    return(
+        <div id="article">
+            <div className="container">
+                <h1>Best Books to Learn Front-End Web Development</h1>
+                <p>
+                There is a huge demand for Front-End Web Developers or Web Designers in IT and Front-End Developer Jobs are also one of the high paid jobs. These all are the reason people love to choose this field. Frontend development is all about UI/UX where the main concern is related to layout, styling or designing of the website.
+                Every web designer or frontend developer journey start from HTML and CSS and after a better understanding of both, they need to jump into Javascript to add interactive features in your HTML and CSS design. When it comes to learning method for frontend development most of the people love to go with online videos or materials however sometimes for reference or to understand the concept in-depth from beginning till the end books are the best options.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Article;
