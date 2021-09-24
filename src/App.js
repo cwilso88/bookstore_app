0f15a318caf244defcfbb0cade7a185f78a589ca
cwilso88 <chasity.wilson4@gmail.com>
2021-09-24 16:46:44 -0400

    Header update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Books from './components/Books';
 import Mylist from './components/Mylist';
 import Modal from './components/Modal';
+import Header from './components/Header';
 import './css/Books.css';
 import './css/Mylist.css';
 import './css/Modal.css';
+import './css/Header.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Books />
       <Mylist />
       <Modal />
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Header = () => {
+    return(
+        <div id="header">
+            <div className="container">
+                <div className="logo"></div>
+                <div className="logo-title">Chasity De La Cruz</div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
