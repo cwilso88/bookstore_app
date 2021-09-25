931abf49b8d9e81e8d6de218616eeefc56cc5294
cwilso88 <chasity.wilson4@gmail.com>
2021-09-24 21:26:58 -0400

    Article and Booklist media queries

--- src/components/Article.js
+++ src/components/Article.js
@@ -9,6 +9,7 @@ const Article = () => {
                 There is a huge demand for Front-End Web Developers or Web Designers in IT and Front-End Developer Jobs are also one of the high paid jobs. These all are the reason people love to choose this field. Frontend development is all about UI/UX where the main concern is related to layout, styling or designing of the website.
                 Every web designer or frontend developer journey start from HTML and CSS and after a better understanding of both, they need to jump into Javascript to add interactive features in your HTML and CSS design. When it comes to learning method for frontend development most of the people love to go with online videos or materials however sometimes for reference or to understand the concept in-depth from beginning till the end books are the best options.
                 </p>
+                <h4>Click on a book to learn more and add it to your list.</h4>
             </div>
         </div>
     )
